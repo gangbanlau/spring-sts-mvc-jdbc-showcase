c9ac35dc6f0e2bc8ec6d7155811c68bb9c752d27
gangban.lau@gmail.com <gangban.lau@gmail.com@cb30764c-6259-e1d8-e74d-5856f48f498a>
2013-03-28 06:11:12 +0000

    @TransactionConfiguration and @Transactional also needed for roll back transaction

--- src/test/java/my/mycompany/myapp/HomeControllerTest.java
+++ src/test/java/my/mycompany/myapp/HomeControllerTest.java
@@ -6,11 +6,13 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.transaction.TransactionConfiguration;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -20,6 +22,8 @@ import org.springframework.web.context.WebApplicationContext;
 @WebAppConfiguration
 @ContextConfiguration(locations = { "classpath*:root-context.xml",
 		"classpath*:servlet-context.xml", "classpath*:datasource.xml" })
+@TransactionConfiguration
+@Transactional
 public class HomeControllerTest {
 
 	@Autowired
