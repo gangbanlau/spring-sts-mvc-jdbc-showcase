f9f64ab8824fb494e5e6965f8682ba386f5e17cf
dengp29@gmail.com <dengp29@gmail.com@cb30764c-6259-e1d8-e74d-5856f48f498a>
2014-03-13 08:28:57 +0000

    添加schedule 假日优惠

---
+++ trunk/src/main/java/my/mycompany/myapp/service/impl/ScheduledJob.java
@@ -0,0 +1,53 @@
+package my.mycompany.myapp.service.impl;
+
+import java.util.List;
+
+import my.mycompany.myapp.domain.Product;
+import my.mycompany.myapp.repository.IProductDao;
+import my.mycompany.myapp.service.IScheduledJob;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ScheduledJob implements IScheduledJob {
+	 private static final Logger logger= LoggerFactory.getLogger(ScheduledJob.class);
+	 @Autowired
+	 private IProductDao productDao;
+
+	    @Scheduled(cron = "0 0 0  ? * 7")
+	    @Override
+	    public void RunPreferentialPrice() {
+	      logger.info("Execute RunPreferentialPrice!");
+	      List<Product> products = productDao.findAll();
+			if (products != null) {
+				for (Product product : products) {
+					double newPrice = product.getPrice().doubleValue() * (100 - 15) / 100;
+					product.setPrice(newPrice);
+					productDao.update(product);
+				}
+			} else {
+				logger.warn("no product to increase price");
+			}
+	    
+	    }
+
+	    @Scheduled(cron = "0 0 0 ? * 2")
+	    @Override
+	    public void RestorePrice() {
+	      logger.info("Execute RestorePrice!");
+	      List<Product> products = productDao.findAll();
+			if (products != null) {
+				for (Product product : products) {
+					double newPrice = product.getPrice().doubleValue() * (100 + 15) / 100;
+					product.setPrice(newPrice);
+					productDao.update(product);
+				}
+			} else {
+				logger.warn("no product to increase price");
+			}
+	    }
+}
